ec1f12e85169f3117bd31f0a2c307550f14d27c6
Dharmik0121 <104291062+Dharmik0121@users.noreply.github.com>
2023-10-01 22:18:10 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,33 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTmKovxpKGw6_eZ3clcUiv22b8_RkUfvI",
+  authDomain: "real-estate-77871.firebaseapp.com",
+  projectId: "real-estate-77871",
+  storageBucket: "real-estate-77871.appspot.com",
+  messagingSenderId: "355345937656",
+  appId: "1:355345937656:web:9028fdee708ef08510370d",
+  measurementId: "G-58Q39PQ3LN"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
